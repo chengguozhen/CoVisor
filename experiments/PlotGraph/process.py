79cdbcaeed24231ea0bba58ac094a56dd3b44c0b
xinjin <jxinpku@gmail.com>
2014-09-04 20:45:17 -0400

    update

---
+++ experiments/PlotGraph/process.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+import sys
+import time
+import random
+
+def readSwitchTime(fileName = "switch_time.txt"):
+    switchTime = list()
+    fin = open(fileName, 'r')
+    oneline = fin.readline()
+    while oneline != "":
+        switchTime.append(float(oneline) / 300)
+        oneline = fin.readline()
+    fin.close()
+    return switchTime
+
+def generateTime(inFile, outFile, switchTime, rounds = 100):
+    fin = open(inFile, 'r')
+    rules = list()
+    oneline = fin.readline()
+    while oneline != "":
+        rules.append(int(oneline))
+        oneline = fin.readline()
+    fin.close()
+
+    fout = open(outFile, 'w')
+    for rule in rules:
+        updateTimes = list()
+        for i in range(rounds):
+            updateTime = 0;
+            for j in range(rule):
+                updateTime += switchTime[random.randint(0, len(switchTime)-1)]
+            updateTimes.append(updateTime)
+        updateTimes.sort()
+        fout.write(str(updateTimes[4]) + "\t" + str(updateTimes[49]) + "\t" + \
+            str(updateTimes[94]) + "\n")
+    fout.close()
+
+
+if __name__ == '__main__':
+    switchTime = readSwitchTime()
+    generateTime("log", "res", switchTime)
+
+
